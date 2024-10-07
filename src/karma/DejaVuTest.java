2d878f8920b676198f19f33075753a0ca63dea90
Jacob Jeffries <jjeffrie@nmu.edu>
2024-10-07 10:11:58 -0400

    updated DejaVu Test.

--- src/karma/DejaVuTest.java
+++ src/karma/DejaVuTest.java
@@ -2,60 +2,67 @@ package karma;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static karma.DejaVu.*;
 
 public class DejaVuTest {
 
   @Test
-  public void testFactorial(){
-    assertEquals(24, factorial(4));
-    assertEquals(6, factorial(3));
-    assertEquals(2, factorial(2));
-    assertEquals(1, factorial(1));
-    assertEquals(1, factorial(0));
+  public void testFactorial() {
+    assertEquals(24, DejaVu.factorial(4));
+    assertEquals(6, DejaVu.factorial(3));
+    assertEquals(2, DejaVu.factorial(2));
+    assertEquals(1, DejaVu.factorial(1));
+    assertEquals(1, DejaVu.factorial(0));
   }
 
   @Test
-  public void testCountdown(){
-    countdown(5);
-    countup(5);
+  public void testCountdownAndCountup() {
+    DejaVu.countdown(5);
+    DejaVu.countup(5);
   }
 
+  // Each term is the sum of the preceding two terms.
   @Test
-  public void testFibonaccii(){
-    assertEquals(1, fibonacci(0));
-    assertEquals(1, fibonacci(1));
-    assertEquals(2, fibonacci(2));
-    assertEquals(3, fibonacci(3));
-    assertEquals(5, fibonacci(4));
-    assertEquals(8, fibonacci(5));
+  public void testFibonacci() {
+    assertEquals(1, DejaVu.fibonacci(0));
+    assertEquals(1, DejaVu.fibonacci(1));
+    assertEquals(2, DejaVu.fibonacci(2));
+    assertEquals(3, DejaVu.fibonacci(3));
+    assertEquals(5, DejaVu.fibonacci(4));
+    assertEquals(8, DejaVu.fibonacci(5));
   }
 
-  // In the recursion multiply, you may only use addition.
-  // You may assume both numbers are positive integers.
+  // In the recursive multiply, you may only use addition and recursion.
+  // You may assume both numbers are positive integers
   @Test
-  public void testMultiply(){
-    assertEquals(8, multiply(1,8));
-    assertEquals(8, multiply(8,1));
-    assertEquals(15, multiply(3,5));
+  public void testMultiply() {
+    assertEquals(8, DejaVu.multiply(1, 8));
+    assertEquals(8, DejaVu.multiply(8, 1));
+    assertEquals(15, DejaVu.multiply(3, 5));
   }
 
   // In the recursive power, you may only use multiplication and recursion.
-  // You may assume the base is positive and the power is not negative.
+  // You may assume the base is positive and the power is non-negative.
   @Test
-  public void testPower(){
-    assertEquals(1, power(2,0));
-    assertEquals(1, power(17,0));
-    assertEquals(8, power(2,3));
-    assertEquals(9, power(3,2));
+  public void testPower() {
+    assertEquals(1, DejaVu.power(2, 0));
+    assertEquals(1, DejaVu.power(17, 0));
+    assertEquals(8, DejaVu.power(2, 3));
+    assertEquals(9, DejaVu.power(3, 2));
   }
 
   @Test
-  public void testReverse(){
-    assertEquals("live", reverse("evil"));
-    assertEquals("tool", reverse("loot"));
-    assertEquals("radar", reverse("radar"));
+  public void testReverse() {
+    assertEquals("live", DejaVu.reverse("evil"));
+    assertEquals("tool", DejaVu.reverse("loot"));
+    assertEquals("radar", DejaVu.reverse("radar"));
   }
 
-  // N choose K things.
+  @Test
+  public void testNChooseK() {
+    assertEquals(1, DejaVu.nChooseK(0, 0));
+    assertEquals(1, DejaVu.nChooseK(3, 0));
+    assertEquals(1, DejaVu.nChooseK(4, 4));
+    assertEquals(6, DejaVu.nChooseK(4, 2));
+    assertEquals(4, DejaVu.nChooseK(4, 3));
+  }
 }
